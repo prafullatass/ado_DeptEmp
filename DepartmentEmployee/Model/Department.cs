2ee4fc4030e3638ba9a4c344c28e7b06e73d4980
Prafullata Sonawane <prafullatass@gmail.com>
2019-05-03 13:05:15 -0500

    first commit

---
+++ DepartmentEmployee/Model/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepartmentEmployee.Model
+{
+    class Department
+    {
+        // C# representation of the Department table
+            public int Id { get; set; }
+            public string DeptName { get; set; }
+    }
+}
